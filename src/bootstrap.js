bb12012ecf57310936104d2282b217231e938fc9
Dennis Ruhe <dennis@amazingsystems.nl>
2018-08-10 13:21:52 +0200

    Issue #6: More informative error message on script loading error

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -16,7 +16,10 @@ export default function (options) {
   })
 
   scriptPromise.catch((error) => {
-    console.error('Error loading script', error)
+    const msg = '[vue-matomo] An error occurred trying to load ' + error.target.src + '. ' +
+            'If the file exists you may have an ad- or trackingblocker enabled.'
+
+    console.error(msg)
   })
 
   return scriptPromise
