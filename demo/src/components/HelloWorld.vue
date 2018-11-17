f6fdd67b25487e364ee24a8b21da8a9bc37ba69a
Dennis Ruhe <dennis@amazingsystems.nl>
2018-11-17 23:41:11 +0100

    Initialized demo project

---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to the vue-matomo demo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
