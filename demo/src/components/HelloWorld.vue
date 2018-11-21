f17c4aa56e8827425250b55b378879fbc1e048f1
Dennis Ruhe <dennis@amazingsystems.nl>
2018-11-21 13:21:05 +0100

    Added an example trackEvent

--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -4,13 +4,25 @@
 
     <p>
       <a href="https://www.google.com">External link to google</a>
+    </p>
+    <p>
       <router-link :to="{name: 'Page1'}">Page 1</router-link>
+    </p>
+    <p>
+      <button @click="trackMatomoEvent">
+        Fire matomo event
+      </button>
     </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  methods: {
+    trackMatomoEvent () {
+      this.$matomo.trackEvent('Buttons', 'Click', 'demo', 10)
+    }
+  }
 }
 </script>
