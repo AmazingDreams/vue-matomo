ccb392356abcf2639b7d7b5f6a377372c3d0e5f1
Dennis Ruhe <dennis@amazingsystems.nl>
2019-10-30 15:18:04 +0100

    Updated deps & demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import VueMatomo from 'vue-matomo'
+import VueMatomo from '../../src/index'
 
 Vue.use(VueMatomo, {
   host: 'https://demo.matomo.org',
